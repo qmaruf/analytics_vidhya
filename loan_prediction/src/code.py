291c0f92d6757355f6bc46e4b286c91dfce507d6
Quazi Marufur Rahman <maruf.csdu@gmail.com>
2017-05-28 17:32:39 +0600

    working on new features. features in this code are no longer useful

--- loan_prediction/src/code.py
+++ loan_prediction/src/code.py
@@ -1,86 +1,58 @@
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import log_loss
 from sklearn.model_selection import StratifiedKFold, train_test_split
 import feature_engineering
+from imp import reload
+reload(feature_engineering)
 import numpy as np
 import os
 import pandas as pd
 import time
 import xgboost as xgb
-from imblearn.combine import SMOTEENN
-from sklearn import preprocessing
+from scipy.stats import mode
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 n_splits = 5
-n_rounds = 5000
-random_seed = 1971
+final_accuracy = 0
+random_state = 1971
 
-X, y, test_id, test = feature_engineering.main(overwrite=False)
-
+X, y, test_id, test = feature_engineering.main2(overwrite=False)
+print (X.shape)
+# exit()
 predictions = np.zeros((test.shape[0], n_splits))
-test  = xgb.DMatrix(test)
-
-
-losses = 0
-
-params = {}
-
-params['eta'] = 0.1
-params['eval_metric'] = ['error']
-params['max_depth'] = 12
-params['n_thread'] = 4
-params['objective'] = 'binary:logistic'
-params['seed'] = random_seed
-params['silent'] = 1
-
-
-def train_xgb(x_train, y_train, x_valid, y_valid, params):		
-	# params['scale_pos'] = np.count_nonzero(y_train==0)/np.count_nonzero(y_train==1)
-	xg_train = xgb.DMatrix(x_train, label=y_train)
-	xg_val = xgb.DMatrix(x_valid, label=y_valid)
-	watchlist  = [(xg_train,'train'), (xg_val,'eval')]
-	
-	model = xgb.train(params, xg_train, n_rounds, watchlist, verbose_eval=True, early_stopping_rounds=500) 
-	pred = model.predict(xgb.DMatrix(x_valid), ntree_limit=model.best_ntree_limit)	
-	loss = model.best_score	
-
-	return model, loss
-
-
-
-if n_splits == 5:
-	skf = StratifiedKFold(n_splits=n_splits, random_state=random_seed)
-	for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
-	   X_train, x_valid = X[train_index, :], X[test_index, :]
-	   y_train, y_valid = y[train_index], y[test_index]
-	   model, loss = train_xgb(X_train, y_train, x_valid, y_valid, params)
-	   predictions[:, fold] = model.predict(test)
-	   losses += loss
-	   print ('fold %d Loss %f'%(fold, loss))
-else:
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=random_seed, stratify=y)
-	fold = 0
-	model, loss = train_xgb(X_train, y_train, X_test, y_test, params)
-	predictions[:, fold] = model.predict(test)
-	losses += loss
-
-
-
-prediction = (predictions.mean(axis=1)+0.5).astype(int)
 loan_status = []
-for p in prediction:
-	if p == 1:
-		loan_status.append('Y')
-	else:
-		loan_status.append('N')
-loss = losses/float(n_splits)
+
+model = LogisticRegression()
+# model = svm.SVC()
+# model = KNeighborsClassifier(n_neighbors=3)
+model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.01, max_depth=6, random_state=0)
+
+
+skf = StratifiedKFold(n_splits=n_splits, random_state=random_state)
+for fold, (train_index, test_index) in enumerate(skf.split(X, y)):
+   X_train, x_valid = X[train_index, :], X[test_index, :]
+   y_train, y_valid = y[train_index], y[test_index]
+   model.fit(X_train, y_train)
+   accuracy = accuracy_score(y_valid, model.predict(x_valid))   
+   predictions[:, fold] = model.predict(test)   
+   final_accuracy += accuracy
+   print ('fold %d Accuracy %f'%(fold, accuracy))
+
+final_accuracy /= float(n_splits)
+predictions = mode(predictions, axis=1)[0].astype(int)
+
+for p in predictions:
+	p = 'Y'if p == 1 else 'N'
+	loan_status.append(p)
 
 submission = pd.DataFrame({'Loan_ID': test_id, 'Loan_Status': loan_status})
-sub_file = '../output/%f__%s.csv'%(loss, str(time.ctime()).replace(' ', '_'))
+sub_file = '../output/%f__%s.csv'%(final_accuracy, str(time.ctime()).replace(' ', '_'))
 submission.to_csv(sub_file, index=False)
 
 sub_file = '../output/sample_submission.csv'
 submission.to_csv(sub_file, index=False)
-print ('Final loss %f'%loss)
-
-# cmd = '7z a ../output/submission.7z %s'%sub_file
-# os.system(cmd)
-
+print ('Final loss %f'%final_accuracy)
--- loan_prediction/src/feature_engineering.py
+++ loan_prediction/src/feature_engineering.py
@@ -11,24 +11,66 @@ import operator, os
 import pandas as pd
 import re
 from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.decomposition import PCA
 
 
+def impute(data):
+	for col in data.columns:		
+		data[col] = data[col].fillna('empty') if data[col].dtype == 'object' \
+					else data[col].fillna(data[col].mean())
+	return data
 
-def main(overwrite):
 
-	train = pd.read_csv('../input/train_u6lujuX_CVtuZ9i.csv')
-	test = pd.read_csv('../input/test_Y3wMUE5_7gLdaTN.csv')
+def main2(overwrite):
+
+	train = pd.read_csv('../input/train.csv')
+	test = pd.read_csv('../input/test.csv')
 	test_id = test.Loan_ID
 
 	y = train.Loan_Status
 
 	mp = {'Y': 1, 'N': 0}
 	y = y.map(mp)
-	train.drop(['Loan_Status', 'Loan_ID'], axis=1, inplace=True)
+	train.drop(['Loan_Status'], axis=1, inplace=True)
 
 	data = pd.concat([train, test])
+	data.drop(['Loan_ID'], axis=1, inplace=True)
 
-	data = data.fillna(-1)
+	data = impute(data)	
+
+	features = pd.DataFrame()
+	features['Credit_History'] = data['Credit_History']
+	features['Property_Area'] = data['Property_Area']
+	features['LoanAmount/Loan_Amount_Term'] = data['LoanAmount']/data['Loan_Amount_Term']
+	
+	data = pd.get_dummies(features)
+
+	print (data.head())
+
+
+
+	x = data[:train.shape[0]]
+	test = data[train.shape[0]:]
+
+	return x.as_matrix(), y, test_id, test       
+
+def main(overwrite):
+
+	train = pd.read_csv('../input/train.csv')
+	test = pd.read_csv('../input/test.csv')
+	test_id = test.Loan_ID
+
+	y = train.Loan_Status
+
+	mp = {'Y': 1, 'N': 0}
+	y = y.map(mp)
+	train.drop(['Loan_Status'], axis=1, inplace=True)
+
+	data = pd.concat([train, test])
+	data.drop(['Loan_ID'], axis=1, inplace=True)
+
+	data = impute(data)	
 	data = pd.get_dummies(data)
 
 	scaler = preprocessing.MinMaxScaler().fit(data)
@@ -37,7 +79,4 @@ def main(overwrite):
 	x = data[:train.shape[0]]
 	test = data[train.shape[0]:]
 
-	return x, y, test_id, test        
-
-
-    
+	return x, y, test_id, test       
